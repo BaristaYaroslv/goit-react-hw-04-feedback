559d47c0e9281cfb3f77777fd98b4bb318174714
Yaroslav <110727630+BaristaYaroslv@users.noreply.github.com>
2023-03-27 21:36:01 +0300

    class>>> hooks

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -8,7 +8,7 @@ export function FeedbackOptions({options, onLeaveFeedback}) {
             {options.map(option => {
                 return (
                     <li key={option}>
-                        <Button  type="button" name={option} onClick={() => {onLeaveFeedback(option);}}>
+                        <Button  type="button" name={option} onClick={onLeaveFeedback}>
                             {option}
                         </Button>
                     </li>
